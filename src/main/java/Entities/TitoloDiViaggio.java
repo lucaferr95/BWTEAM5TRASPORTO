240fb03099acf395dc47d76642269c97babb8a36
LLeo93 <liba.leoncini@gmail.com>
2025-05-27 09:59:20 +0200

    biglietto abbonamento titolodiviaggio

--- src/main/java/Entities/TitoloDiViaggio.java
+++ src/main/java/Entities/TitoloDiViaggio.java
@@ -1,4 +1,41 @@
 package Entities;
 
-public class TitoloDiViaggio {
+import java.time.LocalDate;
+
+public abstract class TitoloDiViaggio {
+
+    protected LocalDate dataEmissione;
+    protected Rivenditore rivenditore;
+
+    public TitoloDiViaggio() {
+    }
+
+    public TitoloDiViaggio(LocalDate dataEmissione, Rivenditore rivenditore) {
+        this.dataEmissione = dataEmissione;
+        this.rivenditore = rivenditore;
+    }
+
+    public LocalDate getDataEmissione() {
+        return dataEmissione;
+    }
+
+    public void setDataEmissione(LocalDate dataEmissione) {
+        this.dataEmissione = dataEmissione;
+    }
+
+    public Rivenditore getRivenditore() {
+        return rivenditore;
+    }
+
+    public void setRivenditore(Rivenditore rivenditore) {
+        this.rivenditore = rivenditore;
+    }
+
+    @Override
+    public String toString() {
+        return "TitoloDiViaggio{" +
+                "dataEmissione=" + dataEmissione +
+                ", rivenditore=" + (rivenditore != null ? rivenditore.getId() : null) +
+                '}';
+    }
 }
